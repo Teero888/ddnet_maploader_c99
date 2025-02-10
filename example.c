58f65e63eb56a40b9cbaaa79deaf6a9aab2775fc
Teero888 <teero484@gmail.com>
2025-02-10 16:39:50 +0100

    add example

---
+++ example.c
@@ -0,0 +1,17 @@
+#include "map_loader.h"
+#include <stdio.h>
+
+int main() {
+  GameTiles map_data = load_map("/home/teero/.local/share/ddnet/maps/za1e.map");
+
+  if (!map_data.m_pData)
+    return 1;
+
+  printf("Map loaded successfully!\n");
+  // Get tile at x: 24, y: 10
+  printf("Tile: %d\n", map_data.m_pData[10 * map_data.m_Width + 24]);
+
+  free_map_data(&map_data);
+  return 0;
+}
+// compile with gcc example.c map_loader.c -lz
